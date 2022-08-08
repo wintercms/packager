c3bab9a57a3c61948f0f06dee71f2dd72f181dd4
Marc Jauvin <marc.jauvin@gmail.com>
2022-08-08 14:21:08 -0400

    php 7.4 does not support named arguments

--- tests/Cases/Commands/UpdateTest.php
+++ tests/Cases/Commands/UpdateTest.php
@@ -371,8 +371,7 @@ final class UpdateTest extends ComposerTestCase
         $this->copyToWorkDir($this->testBasePath() . '/fixtures/valid/simple/composer.json');
 
         // call with dryRun = true
-        #$update = $this->composer->update(true, false, false, 'none', false, true);
-        $update = $this->composer->update(dryRun: true);
+        $update = $this->composer->update(true, false, false, 'none', false, true);
 
         // make sure no lock file gets created
         $this->assertFileDoesNotExist($this->workDir . '/composer.lock');
