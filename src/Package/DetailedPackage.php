1c082d66f037172eda69fc2009ef398ff59464a3
Ben Thomson <git@alfreido.com>
2024-04-09 08:59:32 +0800

    Add extra details to detailed packages

--- src/Package/DetailedPackage.php
+++ src/Package/DetailedPackage.php
@@ -15,6 +15,9 @@ class DetailedPackage extends Package
      * @param array<string, string> $requires
      * @param array<string, string> $devRequires
      * @param array<string, mixed> $extras
+     * @param array<string, string> $suggests
+     * @param array<string, string> $conflicts
+     * @param array<string, string> $replaces
      */
     public function __construct(
         string $namespace,
@@ -30,6 +33,10 @@ class DetailedPackage extends Package
         protected array $requires = [],
         protected array $devRequires = [],
         protected array $extras = [],
+        protected array $suggests = [],
+        protected array $conflicts = [],
+        protected array $replaces = [],
+        protected string $readme = '',
     ) {
         parent::__construct($namespace, $name, $description);
     }
@@ -44,11 +51,17 @@ class DetailedPackage extends Package
         $this->type = $type;
     }
 
+    /**
+     * @return array<int, string>
+     */
     public function getKeywords(): array
     {
         return $this->keywords;
     }
 
+    /**
+     * @param array<int, string> $keywords
+     */
     public function setKeywords(array $keywords): void
     {
         $this->keywords = $keywords;
@@ -64,71 +77,113 @@ class DetailedPackage extends Package
         $this->homepage = $homepage;
     }
 
+    /**
+     * @return array<int, array<string, string>>
+     */
     public function getAuthors(): array
     {
         return $this->authors;
     }
 
+    /**
+     * @param array<int, array<string, string>> $authors
+     */
     public function setAuthors(array $authors): void
     {
         $this->authors = $authors;
     }
 
+    /**
+     * @return array<int, array<string, string>>
+     */
     public function getLicenses(): array
     {
         return $this->licenses;
     }
 
+    /**
+     * @param array<int, array<string, string>> $licenses
+     */
     public function setLicenses(array $licenses): void
     {
         $this->licenses = $licenses;
     }
 
+    /**
+     * @return array<string, string>
+     */
     public function getSupport(): array
     {
         return $this->support;
     }
 
+    /**
+     * @param array<string, string> $support
+     */
     public function setSupport(array $support): void
     {
         $this->support = $support;
     }
 
+    /**
+     * @return array<string, string>
+     */
     public function getFunding(): array
     {
         return $this->funding;
     }
 
+    /**
+     * @param array<string, string> $funding
+     */
     public function setFunding(array $funding): void
     {
         $this->funding = $funding;
     }
 
+    /**
+     * @return array<string, string>
+     */
     public function getRequires(): array
     {
         return $this->requires;
     }
 
+    /**
+     * @param array<string, string> $requires
+     */
     public function setRequires(array $requires): void
     {
         $this->requires = $requires;
     }
 
+    /**
+     * @return array<string, string>
+     */
     public function getDevRequires(): array
     {
         return $this->devRequires;
     }
 
+    /**
+     * @param array<string, string> $devRequires
+     */
     public function setDevRequires(array $devRequires): void
     {
         $this->devRequires = $devRequires;
     }
 
+    /**
+     * @return array<string, mixed>
+     */
     public function getExtras(): array
     {
         return $this->extras;
     }
 
+    /**
+     * @param array<string, mixed> $extras
+     */
     public function setExtras(array $extras): void
     {
         $this->extras = $extras;
